9d4cdc15b407128129a4d567b5f881e7ebd4e75b
callor <callor@callor.com>
2025-02-18 17:31:29 +0900

    routes

--- hello-fresh-01/fresh.gen.ts
+++ hello-fresh-01/fresh.gen.ts
@@ -5,6 +5,7 @@
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $api_joke from "./routes/api/joke.ts";
+import * as $book_index from "./routes/book/index.tsx";
 import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $home_index from "./routes/home/index.tsx";
 import * as $index from "./routes/index.tsx";
@@ -16,6 +17,7 @@ const manifest = {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
     "./routes/api/joke.ts": $api_joke,
+    "./routes/book/index.tsx": $book_index,
     "./routes/greet/[name].tsx": $greet_name_,
     "./routes/home/index.tsx": $home_index,
     "./routes/index.tsx": $index,
---
+++ hello-fresh-01/routes/book/index.tsx
@@ -0,0 +1,4 @@
+export default function HomePage() {
+  const time = new Date().toLocaleString();
+  return <h1>대한민국 책방입니다 {time}</h1>;
+}
